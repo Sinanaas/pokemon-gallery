f1b03a7826838fdf707b740bd209140a6694ca8b
M Sinan Abdul Syakur <84369115+Sinanaas@users.noreply.github.com>
2023-08-08 12:15:29 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Pokemon Gallery</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
